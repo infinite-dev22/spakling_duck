a97f358bac964896fa767011ca0a07d204e5adc8
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-03-07 12:36:55 +0300

    Separate employee item widget from employee layout

---
+++ lib/ui/pages/employees/layout/employee_item_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:smart_rent/ui/themes/app_theme.dart';
+import 'package:smart_rent/ui/widgets/user_card_widget.dart';
+
+class EmployeeItemWidget extends StatelessWidget {
+  const EmployeeItemWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildBody();
+  }
+
+  _buildBody() {
+    return Slidable(
+        key: const ValueKey(0),
+        startActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          dismissible: DismissiblePane(onDismissed: () {}),
+          children: [
+            SlidableAction(
+              onPressed: (context) {},
+              backgroundColor: Colors.transparent,
+              foregroundColor: AppTheme.blue,
+              icon: FontAwesome.pen_to_square,
+            ),
+          ],
+        ),
+        endActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          dismissible: DismissiblePane(onDismissed: () {}),
+          children: [
+            SlidableAction(
+              onPressed: (context) {},
+              backgroundColor: Colors.transparent,
+              foregroundColor: AppTheme.red,
+              icon: FontAwesome.trash_can,
+            ),
+          ],
+        ),
+        child: const UserCardWidget());
+  }
+}
--- lib/ui/pages/employees/layout/employee_page_layout.dart
+++ lib/ui/pages/employees/layout/employee_page_layout.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:smart_rent/ui/pages/employees/layout/employee_item_widget.dart';
 import 'package:smart_rent/ui/themes/app_theme.dart';
 import 'package:smart_rent/ui/widgets/appbar_content.dart';
-import 'package:smart_rent/ui/widgets/user_card_widget.dart';
 
 class EmployeePageLayout extends StatelessWidget {
   const EmployeePageLayout({super.key});
@@ -15,7 +14,7 @@ class EmployeePageLayout extends StatelessWidget {
   Widget _employeePageLayout() {
     return Scaffold(
       appBar: AppBar(
-        title: TitleBarImageHolder(),
+        title: const TitleBarImageHolder(),
         backgroundColor: AppTheme.primaryColor,
         centerTitle: true,
       ),
@@ -23,47 +22,8 @@ class EmployeePageLayout extends StatelessWidget {
           shrinkWrap: true,
           itemCount: 20,
           itemBuilder: (context, index) {
-            return Slidable(
-                // Specify a key if the Slidable is dismissible.
-                key: const ValueKey(0),
-
-                // The start action pane is the one at the left or the top side.
-                startActionPane: ActionPane(
-                  // A motion is a widget used to control how the pane animates.
-                  motion: const ScrollMotion(),
-
-                  // A pane can dismiss the Slidable.
-                  dismissible: DismissiblePane(onDismissed: () {}),
-
-                  // All actions are defined in the children parameter.
-                  children: [
-                    // A SlidableAction can have an icon and/or a label.
-                    SlidableAction(
-                      onPressed: makeCall1(),
-                      icon: Icons.delete,
-                      label: 'Delete',
-                      foregroundColor: Colors.red,
-                    ),
-                    SlidableAction(
-                      onPressed: makeCall2(),
-                      icon: Icons.share,
-                      label: 'Share',
-                      foregroundColor: Colors.green,
-                    ),
-                  ],
-                ),
-
-                // The end action pane is the one at the right or the bottom side.
-                child: UserCardWidget());
+            return const EmployeeItemWidget();
           }),
     );
   }
-
-  makeCall1() {}
-
-  makeCall2() {}
-
-  makeCall3() {}
-
-  makeCall4() {}
 }
