3a7659718fd7d9563bbe068efece0e9b0bcd533c
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-03-08 12:52:06 +0300

    Improve units ui

--- lib/ui/pages/propert_details/screens/units_success_screen.dart
+++ lib/ui/pages/propert_details/screens/units_success_screen.dart
@@ -5,50 +5,39 @@ import 'package:smart_rent/ui/themes/app_theme.dart';
 
 class UnitsSuccessScreen extends StatelessWidget {
   final Property property;
+
   const UnitsSuccessScreen({super.key, required this.property});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-
+    return Scaffold(
       floatingActionButton: FloatingActionButton.extended(
-        splashColor: Colors.transparent,
-        elevation: 0.0,
-        heroTag: null,
-        label: GestureDetector(
-          onTap: () {
-            showModalBottomSheet(
-                useSafeArea: true,
-                isScrollControlled: true,
-                context: context,
-                builder: (context) {
-                  return AddUnitForm(
-                    addButtonText: 'Add',
-                    isUpdate: false,
-                    property: property,
-
-                  );
-                });
-          },
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: AppTheme.primaryColor
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.add, color: AppTheme.whiteColor,),
-            ),
+        heroTag: "add_unit",
+        label: const Text(
+          "Add Unit",
+          style: TextStyle(
+            color: Colors.white,
           ),
         ),
-        onPressed: () {},
-        backgroundColor: Colors.transparent,
+        icon: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () => showModalBottomSheet(
+            useSafeArea: true,
+            isScrollControlled: true,
+            context: context,
+            builder: (context) {
+              return AddUnitForm(
+                addButtonText: 'Add',
+                isUpdate: false,
+                property: property,
+              );
+            }),
+        backgroundColor: AppTheme.primary,
       ),
-
-      body: Column(
-        children: [
-
-        ],
+      body: const Column(
+        children: [],
       ),
     );
   }
